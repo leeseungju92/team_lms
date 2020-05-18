7a351359a73935c42c3533298e241e0ee9c3285f
kojaehun <rhwogns01@gmail.com>
2020-05-18 14:43:57 +0900

    Revert "고재훈"
    This reverts commit 4fc36934d4e6928a6dff7a3489a8a2df0b85c447.

--- lms/src/main/java/com/lms/controller/BoardController.java
+++ lms/src/main/java/com/lms/controller/BoardController.java
@@ -1,16 +1,8 @@
 package com.lms.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.lms.domain.BoardDTO;
-import com.lms.service.BoardService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,33 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/board")
 public class BoardController {
 	
-	@Autowired
-	BoardService bService;
-	
 	@GetMapping("/list")
-	public String boardlist(Model model) {
-		
-		List<BoardDTO> list = bService.boardlist();
-		model.addAttribute("list", list);
+	public String boardlist() {
 		
 		log.info(">>>>> GET BoardList page");
 		return "board/list";
 	}
-	
-	@GetMapping("/register")
-	public String boardregister() {
-		log.info(">>>>> GET Board Regsiter Page");
-		return "board/register";
-	}
-	
-	@PostMapping("/register")
-	public String boardregister(BoardDTO bDto) {
-		log.info(">>>>> 게시판글쓰기 등록~");
-		log.info(bDto.toString());
-		
-		
-		bService.boardRegister(bDto);
-		return "board/list";
-	}
-	
 }
